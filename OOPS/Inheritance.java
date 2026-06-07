bf0ecbff12ae6e42b0873bfc8028fe2c790e8ddc
Aayush Mani <aayushmani1449f@gmail.com>
2026-06-07 15:53:03 +0530

    OOPS implemented

---
+++ OOPS/Inheritance.java
@@ -0,0 +1,21 @@
+package OOPS;
+
+class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat(); // Inherited method
+        dog.bark();
+    }
+}
